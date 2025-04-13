17827a0ebf414c1bfaa75d22017fe946df60b9f5
kanghwan <rkdghks9810@gmail.com>
2025-04-13 15:43:23 +0900

    main - Patch Version 2_Brand New

--- test.c
+++ test.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-#define Version 1
+#define Version 2
 
 int main() {
-    int a = 1;
-    printf("%d\n", a);
-    printf("Hello World!\n");
-    printf("Patch Version : %d.1.1\n", Version);
-    printf("I am curious about this result!\n");
+    printf("Patch Version : %d\n", Version);
+    printf("It's a brand new version patch!\n");
+    printf("I think I understand a little bit now lol~\n");
     return 0;
 }
