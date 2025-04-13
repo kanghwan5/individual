4a54dff9119a951f27b348f2e51abfaed4bbe34e
kanghwan <rkdghks9810@gmail.com>
2025-04-13 15:37:25 +0900

    Branch 1.1.1 - Patch   - I am curious about this result!

--- test.c
+++ test.c
@@ -5,6 +5,7 @@ int main() {
     int a = 1;
     printf("%d\n", a);
     printf("Hello World!\n");
-    printf("Patch Version : %d.1\n", Version);
+    printf("Patch Version : %d.1.1\n", Version);
+    printf("I am curious about this result!\n");
     return 0;
 }
